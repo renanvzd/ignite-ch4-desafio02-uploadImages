fd5f7eff5339b3bd21ab23d2cf4480d50ccec814
Renan Veronez Drechsler <renanvz@outlook.com>
2022-01-17 15:38:20 -0300

    fetchImage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,29 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface GetImagesResponse {
+  after: string;
+  data: Image[];
+}
+
 export default function Home(): JSX.Element {
+  async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
+    const { data } = await api('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return data;
+  }
+
   const {
     data,
     isLoading,
@@ -18,15 +40,30 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
+    fetchImages,
+    {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    }
     // TODO AXIOS REQUEST WITH PARAM
-    ,
     // TODO GET AND RETURN NEXT PAGE PARAM
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+
+    return formatted;
   }, [data]);
 
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
+
+  if (isLoading && isError) {
+    return <Error />;
+  }
   // TODO RENDER LOADING SCREEN
 
   // TODO RENDER ERROR SCREEN
